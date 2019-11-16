dd1519e33c085497049dfd7a338705cb9a974af4
Stephen Sanchez <stephensanchez05@gmail.com>
2019-11-16 02:20:49 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,13 @@
 def prime?(numbers)
   prime = true
-numbers.each do |number|
-  if number.even?
+  i = 0
+  new_array = []
+  while i < numbers.length
+  new_array = [] << yield(collection[i])
+    i = i + 1 
+  end
+  if new_array.include?(2)
     prime = false 
   end
 end
-
-prime
-    
+  
