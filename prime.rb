cb60c8867003e807e2769fb62393a8d8d9a64378
Stephen Sanchez <stephensanchez05@gmail.com>
2019-11-16 02:24:26 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,13 +1,5 @@
-def prime?(numbers)
-  prime = false
-  i = 0
-  new_array = []
-  while i < numbers.length
-  new_array = [] << yield(numbers[i])
-    i = i + 1 
-  end
-  if new_array.include?(2)
-    prime = true
-  end
+def prime?(n)
+  return false if n < 2
+
+  (2..n/2).none?{|i| n % i == 0}
 end
-  
